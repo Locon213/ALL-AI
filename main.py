22dd04fd4b2a3058a7c57df62ace984b4701f3f8
Locon213 <167447571+Locon213@users.noreply.github.com>
2024-12-18 20:35:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,13 +11,15 @@ from telegram.request import HTTPXRequest
 from flask import Flask, request, jsonify
 from threading import Thread
 import asyncio
+import httpx
 
 # Создайте объект HTTPXRequest
 httpx_request = HTTPXRequest(
-    read_timeout=120,
-    write_timeout=120,
-    connect_timeout=120,
-    http_version="2"  # Используем HTTP/2 для более эффективного использования соединений
+    read_timeout=300,
+    write_timeout=300,
+    connect_timeout=300,
+    http_version="2",  # Используем HTTP/2 для более эффективного использования соединений
+    limits=httpx.Limits(max_connections=100, max_keepalive_connections=20)  # Увеличиваем количество соединений
 )
 
 # Логирование
